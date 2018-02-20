8d580b2d84fdb1dc0efeb14c2c813e5d3dbca50f
melissayauri <yaurimelissa@gmail.com>
2018-02-20 16:44:59 -0500

    funcionalidad para publicar un mensaje

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,47 @@
 $(document).ready(function() {
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
+  /* Petición con ajax*/
+  let objetoAJAX = $.get('index.html', function() {
 
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
+  });
+  /* se ejecuta cuando hay respuesta correcta de la solicitud en ajax*/
+  objetoAJAX.done(function() {
+    /* variables generales*/
+    /* variable del container de publicaciones*/
+    let publications = $('#publications');
+    /* modal para publicar el mensaje*/
+    $('#message').modal();
+    /* evento para publicar el mensaje*/
+    $('#postMessage').on('click', postMessage);
+    /* función para publicar el mensaje*/
+    function postMessage() {
+      /* variable para el título*/
+      let title = $('#title').val();
+      /* variable para el mensaje*/
+      let message = $('#post').val();
+      /* varibale para la tarjeta del mensaje*/
+      let cardMessage = `<div class="row">
+      <div class="col s12 l6 offset-l3">
+      <div class="card red accent-1 hoverable">
+      <div class="card-content black-text">
+      <h1 class="card-title white-text title">${title}</h1>
+      <p>${ message}</p>
+      </div>
+      </div>
+      </div>
+      </div>`;
+      /* incorporando la tarjeta*/
+      publications.prepend(cardMessage);
+      /* limpiando las entradas de la tarjeta de mensaje*/
+      $('#title').val('');
+      $('#post').val('');
+    };
+    /* verifica si hay error*/
+    objetoAJAX.fail(function() {
+      alert('verifica de nuevo');
+    });
+    // hacer algo tanto en error como en éxito
+    objetoAJAX.always(function() {
+      console.log('esta hecho');
+    });
   });
 });
